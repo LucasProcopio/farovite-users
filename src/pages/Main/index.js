d1e5bb84db3655edb89148753ea7a46e8838b9a3
Lucas Procopio <lucas@eadskill.com.br>
2019-09-19 16:23:56 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newUser: '',
+      users: [],
+    };
+  }
+
+  handleAddUser = async () => {
+    const { newUser, users } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const userDate = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, userDate],
+      newUser: '',
+    });
+  };
+
+  render() {
+    const { newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Add user"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+Main.navigationOptions = {
+  title: 'Users',
+};
+
+export default Main;
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
