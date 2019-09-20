71811a31c130cb170838b68cd44b0bf71fd75e4c
Lucas Procopio <lucas@eadskill.com.br>
2019-09-20 15:54:10 -0300

    feat: Add user details

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,77 @@
 import React from 'react';
-import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Title,
+  Author,
+  Info,
+} from './styles';
 
-export default function User() {
-  return <View />;
+class User extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('userData').name,
+  });
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      stars: [],
+    };
+  }
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('userData');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { stars } = this.state;
+    const user = navigation.getParam('userData');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => star.id.toString()}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
+  }
 }
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
+
+export default User;
